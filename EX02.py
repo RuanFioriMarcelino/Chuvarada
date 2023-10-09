134d6aed0c95c3faea94dd7d2b64ec37ca96c823
Ruan <133697229+RuanFioriMarcelino@users.noreply.github.com>
2023-10-09 17:28:33 -0300

    Add files via upload

---
+++ EX02.py
@@ -0,0 +1,93 @@
+import random
+
+def jogar(): 
+    iniciou() 
+    ganhou = False 
+    perdeu = False 
+    velha = False 
+
+    iniciar = 0 
+
+    possiveis() 
+    posicoes = [["_","_","_"], 
+
+                ["_","_","_"], 
+
+                ["_","_","_"]] 
+
+    ordem = jogada(iniciar, posicoes) 
+
+ 
+
+    while (not ganhou and not perdeu and not velha): 
+
+        if(ordem ==0): 
+
+            print("SUA VEZ!") 
+
+            linha = int(input("Insira o numero da linha")) 
+
+            coluna = int(input("Insira o numero da coluna")) 
+
+            posicoes[linha][coluna] = ("X") 
+
+            for n in range(0, 3): 
+
+                print(posicoes[n]) 
+
+            ordem = ordem + 1 
+
+            print("\n") 
+
+        else: 
+
+            print("SUA VEZ!") 
+
+            linha = int(input("Insira o numero da linha")) 
+
+            coluna = int(input("Insira o numero da coluna")) 
+
+            posicoes[linha][coluna] = ("O") 
+
+            for n in range(0, 3): 
+
+                print(posicoes[n]) 
+
+            ordem = ordem - 1 
+
+            print("\n") 
+
+
+def iniciou(): 
+  print("Bem vindo ao jogo da velha!!!\n") 
+  pgt=input("Pressione 'F' para inicar o Jogo \nDigite 'Sair' para encerrar\n").upper()
+
+
+ 
+
+def possiveis(): 
+    print("0,0","|","0,1","|","0,2") 
+    print("1,0","|","1,1","|","1,2") 
+    print("2,0","|","2,1","|","2,2") 
+
+def jogada( start, posicoes): 
+    if(start == 1): 
+
+        print("Vez do Jogador 1") 
+        linha = int(input("Linha: ")) 
+        coluna = int(input("Coluna: ")) 
+        posicoes[linha][coluna] = ("X") 
+        for n in range(0,3): 
+            print(posicoes[n]) 
+        ordem = 1 
+
+    else: 
+        print("Vez do Jogador 2") 
+        linha = int(input("Linha: ")) 
+        coluna = int(input("Coluna: ")) 
+        posicoes[linha][coluna] = ("O") 
+        for n in range(0,3): 
+            print(posicoes[n]) 
+        ordem = 0  
+    return ordem 
+jogar()
